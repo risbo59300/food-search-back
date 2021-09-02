758fccf1731518f75493f5584e8f1525a86f0c91
Alexandre Repaire-Carlier <repaire.alexandre@gmail.com>
2021-09-02 15:29:36 +0200

    création crud platsRestaurant

---
+++ src/main/java/com/foodsearchback/controllers/PlatsRestaurantController.java
@@ -0,0 +1,42 @@
+package com.foodsearchback.controllers;
+
+import com.foodsearchback.entities.FsPlatsRestaurant;
+import com.foodsearchback.services.PlatsRestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/platsRestaurant")
+public class PlatsRestaurantController {
+
+    @Autowired
+    PlatsRestaurantService platsRestaurantService;
+
+    @GetMapping("/find")
+    public List<FsPlatsRestaurant> find() {
+        return platsRestaurantService.findAll();
+    }
+
+    @GetMapping("/specialite/{id}")
+    public List<FsPlatsRestaurant> findById() {
+        return platsRestaurantService.findAll();
+    }
+
+    @PostMapping("/add")
+    public void add(@RequestBody FsPlatsRestaurant platsRestaurant) {
+        platsRestaurantService.add(platsRestaurant);
+    }
+
+    @PutMapping("/update")
+    public void update(@RequestBody FsPlatsRestaurant platsRestaurant) {
+        platsRestaurantService.update(platsRestaurant);
+    }
+
+    @DeleteMapping("/delete")
+    public void delete(@PathVariable long id) {
+        platsRestaurantService.delete(platsRestaurantService.findById(id));
+    }
+}
